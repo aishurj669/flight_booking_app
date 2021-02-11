e3d5fb91d982b5487435aadbdae959b046477863
aishurj669 <77617525+aishurj669@users.noreply.github.com>
2021-02-11 14:32:09 +0530

    Add files via upload

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const blue = Color.fromRGBO(31,31,152,1);
-  static const pink = Color.fromRGBO(255,112,162,1);
+  static const backgroundblue = Color.fromRGBO(31,31,152,1);
+  static const buttonpink = Color.fromRGBO(255,112,162,1);
+  static const textwhite = Color.fromRGBO(255,255,255,1);
 
 }
---
+++ lib/common/string.dart
@@ -0,0 +1,12 @@
+
+class MyString {
+  static const navana = "navana";
+  static const air = "air";
+  static const StartBooking = "START BOOKING";
+  static const SignUp = "SIGN UP";
+  static const   List image = [
+    'assets/images/Travel1.jpg',
+    'assets/images/Travel2.jpg',
+    'assets/images/Travel3 (2).jpg',
+  ];
+}
